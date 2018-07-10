dd253987e27e7c13d5d7c84ddbea434c76b36ccb
tonghe <tonghe90@gmail.com>
2018-07-10 13:01:37 +0930

    add reverse_layer cpu version

--- caffe/src/caffe/layers/reverse_axis_layer.cpp
+++ caffe/src/caffe/layers/reverse_axis_layer.cpp
@@ -2,7 +2,15 @@
 
 
 namespace caffe {
-
+template <typename Dtype>
+void reverse_cpu(const int count, const Dtype* from_data, Dtype* to_data, 
+	const int* counts, const int axis_count, const int axis) {
+	for(int index=0; index<count; index++) {
+		int ind=(index/counts[axis])%axis_count;
+		int to_index=counts[axis]*(axis_count-2*ind-1)+index;
+		*(to_data+to_index)=*(from_data+index);
+	}
+}
 template <typename Dtype>
 void ReverseAxisLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
         const vector<Blob<Dtype>*>& top) {
@@ -13,7 +21,7 @@ void ReverseAxisLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
 template <typename Dtype>
 void ReverseAxisLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-	vector<int> shape=bottom[0]->shape();
+vector<int> shape=bottom[0]->shape();
 	axis_=reverse_param_.axis();
 	CHECK_GT(shape.size(), 0)<<this->type()<<" does not support 0 axes blob.";
 	CHECK_GE(axis_, 0)<<"axis must be greater than or equal to 0.";
@@ -28,20 +36,30 @@ void ReverseAxisLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
 		*p=bottom[0]->count(i);
 		p++;
 	}
-	*p=1;
+*p=1;
 }
 
 template <typename Dtype>
 void ReverseAxisLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-		const vector<Blob<Dtype>*>& top) {
-	NOT_IMPLEMENTED;
+    const vector<Blob<Dtype>*>& top) {
+  reverse_cpu<Dtype>(bottom[0]->count(), bottom[0]->cpu_data(), 
+		top[0]->mutable_cpu_data(), bottom_counts_.cpu_data(), 
+bottom[0]->shape(axis_), axis_);
 }
 
 template <typename Dtype>
 void ReverseAxisLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     	const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-	NOT_IMPLEMENTED;
+	if (!propagate_down[0]) {
+		return;
+	}
+	reverse_cpu<Dtype>(bottom[0]->count(), top[0]->cpu_diff(), 
+		bottom[0]->mutable_cpu_diff(), bottom_counts_.cpu_data(), 
+bottom[0]->shape(axis_), axis_);
 }
+#ifdef CPU_ONLY
+STUB_GPU(ReverseLayer);
+#endif
 
 INSTANTIATE_CLASS(ReverseAxisLayer);
 REGISTER_LAYER_CLASS(ReverseAxis);
